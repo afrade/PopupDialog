a1e96ed1ecb504c9c049035255dbaa6eaee51464
antoniofrade <antonio.frade@float.pt>
2021-02-17 18:54:26 +0000

    improve preferredWidth | restore PopupDialogButton

--- PopupDialog/Classes/PopupDialogDefaultButtons.swift
+++ PopupDialog/Classes/PopupDialogDefaultButtons.swift
@@ -27,41 +27,25 @@ import Foundation
 import UIKit
 
 // MARK: Default button
-
 /// Represents the default button for the popup dialog
 public final class DefaultButton: PopupDialogButton {}
 
 // MARK: Cancel button
-
 /// Represents a cancel button for the popup dialog
 public final class CancelButton: PopupDialogButton {
-    /*
+
     override public func setupView() {
         defaultTitleColor = UIColor.lightGray
         super.setupView()
     }
-    */
 }
 
 // MARK: destructive button
-
 /// Represents a destructive button for the popup dialog
 public final class DestructiveButton: PopupDialogButton {
 
     override public func setupView() {
         defaultTitleColor = UIColor.red
-        super.setupView()
-    }
-}
-
-
-// MARK: ActionButton button
-
-/// Represents the main action button for the popup dialog
-public final class ActionButton: PopupDialogButton {
-
-    override public func setupView() {
-        defaultTitleFont = UIFont.boldSystemFont(ofSize: defaultTitleFont.pointSize)
         super.setupView()
     }
 }
